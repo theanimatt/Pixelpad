11f5ad2c127767dedded89642474dcb6378caf1d
Máté Bráder <146343488+theanimatt@users.noreply.github.com>
2025-01-16 14:49:44 +0100

    Priority added

---
+++ app/src/main/java/at/ac/fhstp/pixelpad/di/AppModule.kt
@@ -0,0 +1,20 @@
+package at.ac.fhstp.pixelpad.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideContext(@ApplicationContext context: Context): Context {
+        return context
+    }
+}
